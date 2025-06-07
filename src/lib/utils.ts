cd5f5fb41dad565cb974db4b30e3084ecb69e360
Rohitparakh <rohitparakh4@gmail.com>
2025-06-07 23:53:23 +0530

    Removed lovable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatNumber(num:number) {
+  if (num === null || num === undefined) return '';
+
+  const units = [
+    { value: 1E12, symbol: 'T' },
+    { value: 1E9,  symbol: 'B' },
+    { value: 1E6,  symbol: 'M' },
+    { value: 1E3,  symbol: 'K' }
+  ];
+
+  for (let i = 0; i < units.length; i++) {
+    if (num >= units[i].value) {
+      return (num / units[i].value).toFixed(1).replace(/\.0$/, '') + units[i].symbol;
+    }
+  }
+
+  return num.toString(); // for numbers < 1000
+}
